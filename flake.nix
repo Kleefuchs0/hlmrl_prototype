72741b17a3a3bb88f93fc2d50622ac403174a5fa
Kleefuchs <Kleefuchs@outlook.de>
2026-08-03 00:33:42 +0200

    initial

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            pkg-config
+            wayland
+            wayland-scanner
+            libffi
+            libGL
+            gcc
+            clang-tools
+            clang
+            gnumake
+            cmake
+            valgrind
+            bear
+            libxkbcommon
+          ];
+          env = {
+
+            # Set env variables so we can refer to them from the vim configuration
+            CODELLDB_PATH = "${pkgs.vscode-extensions.vadimcn.vscode-lldb}/share/vscode/extensions/vadimcn.vscode-lldb/adapter/codelldb";
+            LIBLLDB_PATH = "${pkgs.vscode-extensions.vadimcn.vscode-lldb}/share/vscode/extensions/vadimcn.vscode-lldb/lldb/lib/liblldb.so";
+          };
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (
+            with pkgs;
+            [
+              wayland
+              glfw
+              libxkbcommon
+              # any other libraries that need to be dynamically linked to
+            ]
+          );
+        };
+      }
+    );
+}
